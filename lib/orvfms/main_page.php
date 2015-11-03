b6a3a1b7e42f9c17f233988e1b7b4a562db91197
Fernando Silva <fcr@netcabo.pt>
2015-11-03 03:08:45 +0000

    Next event displayed in main screen

--- lib/orvfms/main_page.php
+++ lib/orvfms/main_page.php
@@ -1,7 +1,8 @@
 <?php
 
 function displayMainPage(&$s20Table,$myUrl){
-    
+    global $daysOfWeek;
+
     $ndevs = count($s20Table);
 
  ?>
@@ -13,10 +14,16 @@ function displayMainPage(&$s20Table,$myUrl){
     $bheight = 90 / $ndevs;
 
 // Compute timer margin
-    $tmargin = -$bheight * 0.9;
+//    $tmargin = -$bheight * 0.9; //location:top
 
+// Compute timer next action margin
+    $tmargin_next = -$bheight * 0.22;    //location: bottom
+    $tmargin = $tmargin_next;  
+    
+    
 // Compute timer font size 
-    $fsize = $bheight*0.15; 
+    $fsize = 3; //$bheight*0.15; 
+    $fsize_next = 3; //$fsize * .6;
 //
 // Sort array (in this case, by mac address), such that data is displayed in 
 // a deterministic sequence
@@ -54,9 +61,9 @@ function displayMainPage(&$s20Table,$myUrl){
         // Include field for timer information
         if($devData['timerVal'] != 0)
             if($devData['timerAction'])
-                $color="green";
+                $color="#00BB00";
             else
-                $color="#980000";
+                $color="#EE0000";
         else
             if($devData['switchOffTimer'] > 0)
                 $color = "white";
@@ -69,7 +76,43 @@ function displayMainPage(&$s20Table,$myUrl){
                          color:<?php     echo $color; ?>;
                          font-size:<?php echo $fsize; ?>vh;"></div>
 
+
 <?php
+        $next = getNextAction($mac,$s20Table);
+        $nextAct  = $next[0];
+        if($nextAct >= 0){
+            if($nextAct){
+                $color="#00BB00";
+                $label = "on@ ";
+            }
+            else{
+                $color="#EE0000";
+                $label = "off@ ";
+            }
+
+            $nextTimeStamp = $next[1];
+            $nextDate = getdate($nextTimeStamp);
+            $hour = $nextDate['hours'];
+            $min  = $nextDate['minutes'];
+            $sec    = $nextDate['seconds'];
+            
+            $nextTimeS = sprintf("%02d:%02d:%02d",$hour,$min,$sec);
+            $nextS = $label.$nextTimeS;
+            if($nextTimeStamp-time() > 24*3600){
+                $myWeekDay = $nextDate['wday'] - 1;
+                if($myWeekDay < 0) $myWeekDay += 7;
+                $weekDayName = $daysOfWeek[$myWeekDay];
+                $nextS = $nextS.", ".substr($weekDayName,0,3);
+            }
+?>
+
+
+                        <div class="next" style="top:<?php      echo $tmargin_next ?>vh;
+                          color:<?php     echo $color; ?>;
+                         font-size:<?php echo $fsize_next; ?>vh;"> <?php echo $nextS;?>   </div>
+            
+<?php
+        }        
     } 
 ?>
 </center>
